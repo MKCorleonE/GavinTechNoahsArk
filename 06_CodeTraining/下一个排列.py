e7f0996235c16742e36790f1a9d1886a8c715ff8
liangjiawen <529023573@qq.com>
2026-09-16 15:06:01 +0800

    20260916

--- 06_CodeTraining/下一个排列.py
+++ 06_CodeTraining/下一个排列.py
@@ -32,28 +32,26 @@
 # 最佳答案
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        firstIndex = -1
         n = len(nums)
-        def reverse(nums, i, j):
-            while i < j:
-                nums[i],nums[j] = nums[j], nums[i]
-                i += 1
+
+        # 第一步：从右到左找到第一个小于 nums[i+1] 的数 nums[i]
+        i = n - 2
+        while i >= 0 and nums[i] >= nums[i + 1]:
+            i -= 1
+
+        # 如果找到了，进入第二步；否则跳过第二步，反转整个数组
+        if i >= 0:
+            # 第二步：从右到左找到 nums[i] 右边最小的大于 nums[i] 的数 nums[j]
+            j = n - 1
+            while nums[j] <= nums[i]:
                 j -= 1
-        for i in range(n-2, -1, -1):
-            if nums[i] < nums[i+1]:
-                firstIndex = i
-                break
-        #print(firstIndex)
-        if firstIndex == -1:
-            reverse(nums, 0, n-1)
-            return 
-        secondIndex = -1
-        for i in range(n-1, firstIndex, -1):
-            if nums[i] > nums[firstIndex]:
-                secondIndex = i
-                break
-        nums[firstIndex],nums[secondIndex] = nums[secondIndex], nums[firstIndex]
-        reverse(nums, firstIndex+1, n-1)
+            # 交换 nums[i] 和 nums[j]
+            nums[i], nums[j] = nums[j], nums[i]
+
+        # 第三步：反转 nums[i+1:]（如果上面跳过第二步，此时 i = -1）
+        # nums[i+1:] = nums[i+1:][::-1] 这样写也可以，但空间复杂度不是 O(1) 的
+        left, right = i + 1, n - 1
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
