c8b079a17e71806653001ac54f3811b2efdaa399
liangjiawen <529023573@qq.com>
2026-09-23 14:12:58 +0800

    20260923

---
+++ 06_CodeTraining/找到字符串中所有字母异位词.py
@@ -0,0 +1,84 @@
+"""
+给定两个字符串 s 和 p，找到 s 中所有 p 的 异位词 的子串，返回这些子串的起始索引。不考虑答案输出的顺序。
+
+示例 1:
+输入: s = "cbaebabacd", p = "abc"
+输出: [0,6]
+解释:
+起始索引等于 0 的子串是 "cba", 它是 "abc" 的异位词。
+起始索引等于 6 的子串是 "bac", 它是 "abc" 的异位词。
+
+示例 2:
+输入: s = "abab", p = "ab"
+输出: [0,1,2]
+解释:
+起始索引等于 0 的子串是 "ab", 它是 "ab" 的异位词。
+起始索引等于 1 的子串是 "ba", 它是 "ab" 的异位词。
+起始索引等于 2 的子串是 "ab", 它是 "ab" 的异位词。
+"""
+
+# 最佳答案，使用滑动窗口
+from collections import Counter
+
+def findAnagrams(s: str, p: str):
+    if len(s) < len(p):
+        return []
+    
+    need = Counter(p)
+    window = Counter()
+    left = 0
+    res = []
+    
+    for right, ch in enumerate(s):
+        # 右边界字符加入窗口
+        window[ch] += 1
+        
+        # 窗口长度超过 p 的长度，左边界收缩
+        if right - left + 1 > len(p):
+            left_ch = s[left]
+            window[left_ch] -= 1
+            if window[left_ch] == 0:
+                del window[left_ch]
+            left += 1
+        
+        # 窗口长度正好等于 p 的长度时，判断是否为异位词
+        if right - left + 1 == len(p):
+            if window == need:
+                res.append(left)
+    
+    return res
+
+
+# 笔记
+# 异位词（Anagram）的定义是：
+# 两个字符串包含的字符种类和每个字符出现的次数完全相同，只是顺序可以不同。
+# 注意关键词是 “可以不同”，而不是 “必须不同”。
+
+# 复杂度分析
+# 时间复杂度：O(n)，其中 n 是字符串 s 的长度。
+# 空间复杂度：O(1)，因为字符集的大小是固定的（26个字母）。
+
+# 滑动窗口
+# 使用滑动窗口的方法来解决这个问题。我们维护一个窗口，窗口的大小等于字符串 p 的长度。
+# 在每次移动窗口时，我们更新窗口内的字符计数，并与字符串 p 的字符计数进行比较。
+# 如果两者相等，则说明当前窗口内的子串是字符串 p 的异位词。
+
+# 另外一种写法：
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        if len(s) < len(p):
+            return []
+        p_count = [0] * 26
+        s_count = [0] * 26
+        for i in range(len(p)):
+            p_count[ord(p[i]) - ord('a')] += 1
+            s_count[ord(s[i]) - ord('a')] += 1
+        res = []
+        if s_count == p_count:
+            res.append(0)
+        for i in range(len(p), len(s)):
+            s_count[ord(s[i]) - ord('a')] += 1
+            s_count[ord(s[i - len(p)]) - ord('a')] -= 1
+            if s_count == p_count:
+                res.append(i - len(p) + 1)
+        return res
--- 06_CodeTraining/环形链表.py
+++ 06_CodeTraining/环形链表.py
@@ -16,7 +16,7 @@
 解释：链表中有一个环，其尾部连接到第一个节点。
 """
 
-# 最佳答案
+# 最佳答案，使用哈希表
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, x):
@@ -25,4 +25,40 @@ class ListNode:
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
+        seen = set()             # 创建一个空集合来存储已经访问过的节点
+        while head:
+            if head in seen:
+                return True
+            seen.add(head)       # 将当前节点添加到集合中，对于集合来说 .add() 方法是用来添加一个元素，时间复杂度是 O(1)，不会添加重复的元素。
+            head = head.next
+        return False
+
+# 复杂度分析
+# 时间复杂度：O(n)，其中 n 是链表中的节点数。每个节点最多被访问一次。
+# 空间复杂度：O(n)，其中 n 是链表中的节点数。最坏情况下，链表中没有环，所有节点都被存储在集合中。
+
+# 最佳答案，使用快慢指针
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if not head or not head.next:   # 如果链表为空或者只有一个节点，则不可能有环
+            return False
         
+        slow = head        # 慢指针在后
+        fast = head.next   # 快指针在前
+        
+        while slow != fast:   # 当慢指针和快指针不相遇时，继续循环
+            if not fast or not fast.next:   # 如果快指针到达链表末尾，则说明没有环
+                return False
+            slow = slow.next
+            fast = fast.next.next  # 快指针每次移动两步，如果有环，快指针最终会追上慢指针（套圈）
+            
+        return True
+
+# 复杂度分析
+# 时间复杂度：O(n)，其中 n 是链表中的节点数。每个节点最多被访问一次。
+# 空间复杂度：O(1)，只使用了两个指针。
